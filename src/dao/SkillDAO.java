9fa0e2feba7bdf5ae1bb5dad086d98c7c0330dc3
Likhithanjali-2 <liki2444@gmail.com>
2020-04-08 11:26:24 -0500

    first commit-

--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,34 @@
+package dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO{
+	public Skill getSkillBylD (Long id) throws Exception {
+		
+		Skill skill = null;
+		
+		ConnectionManager cm=new ConnectionManager();
+		Connection con=cm.getConnection();
+		
+		Statement stmt=con.createStatement();
+		String sql="select name from skill where id = "+id;
+		
+		stmt.execute(sql);
+		ResultSet rs=stmt.executeQuery(sql);	
+		
+		while(rs.next()) {
+			skill = new Skill();
+			skill.setSkillName(rs.getString("name"));
+		}
+		
+		rs.close();
+		stmt.close();
+		con.close();
+		return skill;
+		
+	}
+}
